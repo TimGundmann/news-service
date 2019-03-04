ddc9e14a78afc382d6fcf23d4acb08c2fa618aac
Tim Gundmann <tim@gundmann.dk>
2019-03-04 18:59:06 +0100

    url fix

--- src/main/java/dk/gundmann/userclient/UserClient.java
+++ src/main/java/dk/gundmann/userclient/UserClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient(name = "user", url = "${user.url}")
+@FeignClient(name = "news", url = "${news.url}")
 public interface UserClient {
 
 	@RequestMapping(method = RequestMethod.POST, value = "/notification/{type}")
